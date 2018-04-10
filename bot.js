51ee305b1dea2db746925dd08e39a6718d59fccf
notebook <babartosz.bns@gmail.com>
2018-04-11 01:49:56 +0200

    Added memes

--- bot.js
+++ bot.js
@@ -1,9 +1,9 @@
 const Discord = require('discord.js');
 const config = require('./config.json');
 const APPID = "7ab46cfe137338d9856c5fb96c3da511";
-//const token = "7ab46cfe137338d9856c5fb96c3da511";
 var request = require('request');
 
+const prefix = '!';
 // Initialize Discord Bot
 const bot = new Discord.Client();
 
@@ -13,38 +13,50 @@ bot.on('ready', () => {
     console.log(bot.user.tag );
 });
 
-bot.on('message', (msg) => {
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
-    if (msg.content.substring(0, 1) == '!') {
-        var args = msg.content.substring(1).split(' ');
-        var cmd = args[0];
+bot.on('message',   message => {
 
-        args = args.splice(1);
-        switch(cmd) {
-            // !ping
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    var args = message.content.substring(1).split(' ');
+    var command = args[0];
+
+    switch (command) {
             case 'ping':
-                msg.reply('Pong!');
-            break;
+                    message.reply('Pong!');
+                    break;
+
             case 'hi':
-              msg.reply('Welcome ');
-            break;
+                    message.channel.send('Welcome ' + message.author);
+                    break;
+
             case 'date':
-              msg.reply('Welcome ' + new Date());
-            break;
+                    message.reply('Welcome ' + new Date());
+                    break;
+
             case 'weather':
-              if(args[0] != null){
-                var location = args[0];
-                getWeather(location, msg);
-              } else {
-                msg.reply("You stupid, you forgot to type a city name.");
-              }
-              break;
-            // Just add any case commands if you want to..
+                    if(args[1] != null){
+                        var location = args[1];
+                        getWeather(location, message);
+                    } else
+                        message.reply("You stupid, you forgot to type a city name.");
+                    break;
+            case 'meme':
+                    getMeme(message);
+                    break;
+
+            case 'urban':
+                    console.log(args.slice(1));
+                    break;
          }
-     }
+
 });
 
+
+function getMeme(message) {
+    var url = 'https://api.imgflip.com/get_memes';
+
+    sendRequestMeme(url, message);
+}
 
 function getWeather(location, message) {
 
@@ -56,8 +68,23 @@ function getWeather(location, message) {
 
 }
 
+function sendRequestMeme(url, message) {
+    request(url, async function(error, response, body){
+        if (!error && response.statusCode == 200) {
+          console.log('error:', error); // Print the error if one occurred
+          console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+          var toSend = JSON.parse(body);
+          computeMeme(toSend, message);
+      } else {
+        console.log("Error: "+ error);
+        console.log("Response: " + response);
+        message.reply("This meme is to good for you, try later.");
+      }
+    });
+}
+
 function sendRequest(url, message) {
-  request(url, function(error, response, body) {
+  request(url,  function(error, response, body) {
     if (!error && response.statusCode == 200) {
       console.log('error:', error); // Print the error if one occurred
       console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
@@ -74,17 +101,34 @@ function sendRequest(url, message) {
   });
 }
 
+function computeMeme(obj, message) {
+    var size = obj.data.memes.length;
+    console.log("Size of meme array: " + size);
+    var index = Math.floor(Math.random() * size);
+    console.log("Choosen index for meme: "+ index);
+    message.reply(obj.data.memes[index].url);
+}
+
 function computeOutput(obj, message){
   var cityName = obj.name,
       tmp = (obj.main.temp-272.15),
       hum = obj.main.humidity,
       pres = obj.main.pressure;
-
+/*
   var msg = "Weather in " + cityName + "\n" +
             "Temperature: " + tmp + " celcius\n" +
             "Pressure: " + pres + " hPa\n" +
             "Humidity: " + hum +"%";
-  message.reply(msg);
+*/
+    var embed = new Discord.RichEmbed()
+                .setColor('#EFFF00')
+                .setTitle("Weather in " + cityName)
+                .setURL('https://openweathermap.org/')
+                .addField("Temperature", "\t" + tmp + " celcius")
+                .addField("Pressure", "\t" + pres + " hPa")
+                .addField("Humidity", "\t" + hum + "%");
+    console.log(embed);
+  message.channel.send(embed);
 
 }
 
@@ -110,6 +154,7 @@ bot.on('guildMemberAdd', (member) => {
   console.log('What????');
   channel.send(`Welcome on the best server in my home, ${member}`);
   member.addRole('432950017272184843','You are new here.');
+
 });
 
 bot.login(config.token);
